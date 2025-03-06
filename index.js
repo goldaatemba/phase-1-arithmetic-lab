a1fa56405df3428f715b8696ed6484870f344bed
golda <goldaatemba610@gmail.com>
2025-03-06 09:25:41 +0300

    added files

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // Result should be 62
+console.log("Multiply:", multiply);
+
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+console.log("Random:", random);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // Result should be 4
+console.log("Mod:", mod);
+
+const max = Math.max(1, 5, 10, 15, 20); // Highest number should be 20
+console.log("Max:", max);
+
+module.exports = { multiply, random, mod, max };
